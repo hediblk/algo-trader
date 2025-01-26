d8ae7c39fead7c5063817858b0b9e34a13ee114c
Hedi Belkahia <hedibelkahia03@gmail.com>
2025-01-26 02:20:29 -0500

    Add mplfinance integration for enhanced price chart visualization

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import yfinance as yf
 import pandas_ta as ta
+import mplfinance as mpf
 
 
 # INTERVALS = [1m, 2m, 5m, 15m, 30m, 60m, 90m, 1h, 1d, 5d, 1wk, 1mo, 3mo]
@@ -10,24 +11,18 @@ import pandas_ta as ta
 
 
 def get_data(ticker, interval='1d', period='10y'):
-    data = yf.download(ticker, interval=interval, period=period)
-    data['Datetime'] = data.index
+    data = yf.download(ticker, interval=interval, period=period,
+                       rounding=True, multi_level_index=False)
     data.drop(columns=['Adj Close'], inplace=True)
-    data = data[['Datetime', 'Volume', 'Open', 'High', 'Low', 'Close']]
+    data = data[['Volume', 'Open', 'High', 'Low', 'Close']]
     return data
 
 
-def price_chart(tickers, interval='1d', period='10y'):
-    prices = []
-    for ticker in tickers:
-        data = get_data(ticker, interval, period)
-        prices.append(data['Close'])
-    df = pd.concat(prices, axis=1)
-    df.columns = tickers
-    df.plot()
-    plt.xlabel('Date')
-    plt.ylabel('Price')
-    plt.grid()
-    plt.title('Price Chart')
-    plt.show()
+def price_chart(df, type='candle', volume=True, style='yahoo', title='Price chart'):
+    df = df.copy()
+    # df.columns = df.columns.droplevel(1)
+    mpf.plot(df.iloc[-100:, :], type=type,
+             volume=volume, style=style, title=title)
 
+aapl = get_data('AAPL')
+price_chart(aapl)

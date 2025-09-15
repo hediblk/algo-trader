9d8793dfdc620f51488484d904699a739065f550
Hedi Belkahia <hedibelkahia03@gmail.com>
2025-09-15 19:19:44 -0400

    cleanup gitignore and setup

--- README.md
+++ README.md
@@ -2,3 +2,15 @@
 Overhaul of the whole project in progress...
 
 Base project structure is based on a simplified version of the [Cookiecutter Data Science](https://cookiecutter-data-science.drivendata.org) template.
+
+
+
+## Running
+1. Run setup script to create a virtual environment and install dependencies:
+   ```bash
+   ./setup.sh
+   ```  
+2. Activate the virtual environment:
+   ```bash
+   source .venv/bin/activate
+   ```
--- setup.sh
+++ setup.sh
@@ -2,17 +2,17 @@
 
 echo "Removing old virtual environment if it exists..."
 
-if [ -d "venv" ]; then
+if [ -d ".venv" ]; then
 echo "Old virtual environment found. Removing..."
-    rm -rf venv
+    rm -rf .venv
 fi
 
 echo "Creating new virtual environment..."
-python3 -m venv venv
+python3 -m venv .venv
 
-source venv/bin/activate
+source .venv/bin/activate
 
 echo "Installing dependencies..."
 pip install -r requirements.txt
 
-echo "Setup complete. The virtual environment 'venv' is ready and dependencies are installed."
+echo "Setup complete. The virtual environment '.venv' is ready and dependencies are installed."

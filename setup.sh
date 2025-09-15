3bb9838d7ce96a0640d2c26469385e8ceabf1c95
Hedi Belkahia <hedibelkahia03@gmail.com>
2025-09-15 18:33:43 -0400

    add setup script and requirements file

---
+++ setup.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+echo "Removing old virtual environment if it exists..."
+
+if [ -d "venv" ]; then
+echo "Old virtual environment found. Removing..."
+    rm -rf venv
+fi
+
+echo "Creating new virtual environment..."
+python3 -m venv venv
+
+source venv/bin/activate
+
+echo "Installing dependencies..."
+pip install -r requirements.txt
+
+echo "Setup complete. The virtual environment 'venv' is ready and dependencies are installed."
